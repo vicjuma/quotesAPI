5f53b052e9263eba44e26ea12bd58e871ee439b3
= <vicjuma945@gmail.com>
2020-05-24 08:08:12 +0300

    Auto-generated commit

---
+++ precache-manifest.8b8a38ea71748f3de798f658216754f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe2af8d8fe6cf2b3cfcffd433ac0adc",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "3b66cc1a242478500af9",
+    "url": "/random-quote-machine/static/js/2.311a54c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.311a54c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93cb5b0cc80c5ec78355",
+    "url": "/random-quote-machine/static/js/main.c63d36f2.chunk.js"
+  },
+  {
+    "revision": "5d10ac826fadbd9fa6e6",
+    "url": "/random-quote-machine/static/js/runtime-main.62554c6d.js"
+  }
+]);
